872fbadef66124de87a5fd8e97682ee27081b293
Snufty <56336551+Snufty@users.noreply.github.com>
2020-09-02 10:20:55 +0200

    Kristian hjælp

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,85 +1,68 @@
 <template>
   <v-container>
-    
     <v-row align-center class="info_box">
-      <v-col cols="3" xs="12" offset-sm="9" no>
-        <h1>
-          Bagel Shop
-        </h1>
+      <v-col cols="12" xs="12" md="3" offset-sm="9">
+        <h1>Bagel Shop</h1>
         <div>
-          <p>
-            Buy our Bagels
-          </p>
+          <p>Buy our Bagels</p>
         </div>
       </v-col>
     </v-row>
 
     <v-row align-center class="info_box" row wrap no-gutters>
-      <v-col cols="3"  xs="12" offset-sm="1">
-        <h1>
-          Bagel Shop
-        </h1>
+      <v-col cols="12" xs="12" md="3" offset-sm="1">
+        <h1>Bagel Shop</h1>
         <div>
-          <p>
-            Buy our Bagels
-          </p>
+          <p>Buy our Bagels</p>
         </div>
       </v-col>
 
-      <v-col cols="3" xs="12">
-        <h1>
-          Bagel Shop
-        </h1>
+      <v-col cols="12" md="3">
+        <h1>Bagel Shop</h1>
         <div>
-          <p>
-            Buy our Bagels
-          </p>
+          <p>Buy our Bagels</p>
         </div>
       </v-col>
 
-      <v-col cols="3" xs="12">
-        <h1>
-          Bagel Shop
-        </h1>
+      <v-col cols="12" xs="12" md="3">
+        <h1>Bagel Shop</h1>
         <div>
-          <p>
-            Buy our Bagels
-          </p>
+          <p>Buy our Bagels</p>
         </div>
       </v-col>
     </v-row>
-
   </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
 
-
 export default {
-  components: {
-    
-  }
-}
+  components: {},
+};
 </script>
 
 <style lang="scss">
+.info_box .col-12 h1 {
+  @include infobox_mixin(
+    5px,
+    map-get($colorz, hvid),
+    10px,
+    5px,
+    map-get($colorz, hvid)
+  );
+  font-weight: bold;
+  text-transform: uppercase;
+  font-size: 16px;
+  text-align: center;
+}
 
-  .info_box .col h1 {
-    @include infobox_mixin(5px, map-get($colorz, hvid), 10px, 5px, map-get($colorz, hvid));
-    font-weight: bold;
-    text-transform: uppercase;
-    font-size: 16px;
-    text-align: center;
-  }
-  
-  .info_box .col div {
-    background-color: white;
-    padding: 20px;
-  }
+.info_box .col-12 div {
+  background-color: white;
+  padding: 20px;
+}
 
-  .row:first-child {
-    height: 25vh;
-  }
-
+.row:first-child {
+  height: 25vh;
+}
 </style>
