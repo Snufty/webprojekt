e5fbb10d5ff20f179f775846ee21f603a762ba42
Snufty <56336551+Snufty@users.noreply.github.com>
2020-09-08 15:33:39 +0200

    database

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Menu from '../views/Menu.vue'
+import Admin from '../views/Admin.vue'
+import AddNewItems from '../components/admin/AddNewItems.vue'
 
 Vue.use(VueRouter)
 
@@ -23,7 +25,17 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/Admin',
+    name: 'Admin',
+    component: Admin
+  },
+  {
+    path: '/AddNew',
+    name: 'AddNew',
+    component: AddNewItems
+  },
 ]
 
 const router = new VueRouter({
